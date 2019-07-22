5a8b0bd1194c474d2a87009c73ccd8989e542447
yao0013 <yao0013@126.com>
2019-07-22 12:48:57 +0800

    ces

--- qiushibaikeSCA.py
+++ qiushibaikeSCA.py
@@ -1,33 +1,24 @@
 import requests
 import re
 
+url = "https://www.qiushibaike.com/text/"
 
-'''def  prash_list(url)
+resq = requests.get(url=url)
 
-     proxies = {
-      #"http":"http://ip:端口号"
-      "https":"https://113.128.29.232:9999"
-     }'''
+html = resq.text
 
-     url = http://www.qiushibaike.com/text
-     
-     resq = requests.get(url=url，proxies=https://120.83.104.108:9999)
+names = re.findall('<h2>(.*?)</h2>',html,re.S)
 
-     html = resq.text
+contents = re.findall('<span>.\n(.*?)</span>',html,re.S)
 
-     names = re.findall('<h2>(.*?)</h2>',html,re.S)
+fms = re.findall('div class="articleGender(.*?)Icon"',html,re.S)
 
-     contents = re.findall('<span>.\n(.*?)</span>',html,re.S)
+ages = re.findall('div class="articleGender.*?Icon">(.*?)</div',html,re.S)
 
-     fms = re.findall('div class="articleGender(.*?)Icon"',html,re.S)
+laughs = re.findall('<i class="number">(.*?)</i>',html,re.S)
 
-     ages = re.findall('div class="articleGender.*?Icon">(.*?)</div',html,re.S)
-
-     laughs = re.findall('<i class="number">(.*?)</i>',html,re.S)
-
-
-for name in names:
-     print(name.strip())
+'''for name in names:
+    print(name.strip())
 
 for content in contents:
     print(content.replace('<br/>',''))
@@ -39,10 +30,11 @@ for age in ages:
      print(age)
 
 for laugh in laughs:
-     print(laugh)
+     print(laugh)'''
+
 
 #for name, content, fm, age, laugh in zip(names, contents, fms, ages, laughs):
-    print(f" {name} , {actor.strip()} , {time} , 评分：{c}{n} ")
+#   print(f" {name} , {actor.strip()} , {time} , 评分：{c}{n} ")
 
 
 #if __name__ =="main__":
