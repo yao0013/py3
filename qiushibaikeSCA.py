03efeeeab41f126aabfd3cb7bc5e5329e3feec57
yao0013 <myname@mymail.comyao0013@126.com>
2019-07-22 22:25:39 +0800

    修正

--- qiushibaikeSCA.py
+++ qiushibaikeSCA.py
@@ -11,15 +11,15 @@ names = re.findall('<h2>(.*?)</h2>',html,re.S)
 
 contents = re.findall('<span>.\n(.*?)</span>',html,re.S)
 
+
 fms = re.findall('div class="articleGender(.*?)Icon"',html,re.S)
 
 ages = re.findall('div class="articleGender.*?Icon">(.*?)</div',html,re.S)
 
 laughs = re.findall('<i class="number">(.*?)</i>',html,re.S)
 
-
-#for name in names:
- #   print(name.strip())
+for name in names:
+    print(name.strip())
 
 for content in contents:
     print(content.replace('<br/>',''))
@@ -33,4 +33,5 @@ for age in ages:
 for laugh in laughs:
      print(laugh)
 
-     5
+for name, content, laugh in zip(names, contents, laughs):
+    print(f"作者{name}{content}{laugh}")
