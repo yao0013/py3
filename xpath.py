71aa3c7dc93b70651f6cbb9067f35d11f4187723
yao0013 <yao0013@126.com>
2019-07-23 15:00:32 +0800

    待修正

--- xpath.py
+++ xpath.py
@@ -9,9 +9,18 @@ def qs_list(url):
 
     titles = html.xpath("//div[@class='cont']/p[1]/a/b")
 
-    Times =  html.xpath("//p[@class='source']/a[1]")
+    times =  html.xpath("//p[@class='source']/a[1]")
     
     authurs = html.xpath("//p[@class='source']/a[2]")
     
     contents = html.xpath("//div[@class='cont']/div[@id]")
 
+    for title,time,authur,content in zip(titles,times,authurs,contents):
+        print(f"{title}{time}{authur}{content}")
+
+if __name__ == "__main__":
+
+    urls = [f'https://www.gushiwen.org/default_{i}.aspx' for i in range(0,9,1)]
+
+    for url in urls:
+        qs_list(url)
