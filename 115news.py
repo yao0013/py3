932afd6bedb2a9032e3a7a8d9114e321dfb6f3a9
yao0013 <myname@mymail.comyao0013@126.com>
2019-08-01 23:15:24 +0800

    15

--- 115news.py
+++ 115news.py
@@ -1,21 +1,13 @@
 import requests
-import re
 from pyecharts.charts import Bar
+
 headers = {
-    'Cookie':'Hm_lvt_17965aa5d4a3ebae4c8b68932b9557a8=1562639526;acw_tc=784e2c9d15640478198377695e4e9913a130171b3a1b280a6f11b928b51dd3;UID=84838689_A1_1564619000;CID=5bb50052b339dd96a134d015b0f2743f;SEID=d78cb57f6d4365014af1dd328a04969047cbd3f8096913b50448855d7560ae7a1dd1bd0b9a8d67ef1051107c9bca460127b0802a57d21bdec3f0de36;'
+    'Cookie':'acw_tc=784e2c9715632849953358334e541710f26021b61e7425677252f9b6b8098e; UID=84838689_A1_1564663444; CID=26b465a552d8229201fc709f686c0de6; SEID=d61fe3513605ff74b511ef1316fdb1b91b8a200854c7357b6f13129f2a4a1bffd688cfefe6566ace3b07565da6be92de8edc361afc290273931b68a6'
 }
 
-url = 'https://yun.115.com/api/1.0/web/1.0/5/news/glist?start=0&limit=20&search_user_id=&start_time=&to_time=&order=&color=1'
+url = 'https://yun.115.com/api/1.0/web/1.0/5/news/glist?start=0&limit=100&search_user_id=&start_time=&to_time=&order=&color=1'
 
 resq = requests.get(url=url,headers=headers)
 data = resq.json()
-users = data['data']['list']
-user = str(users)
-print(user)
-name = re.findall('user_name\':\'(.*?)\'\,\'is_member',user,re.S)
-print(name)
-print(data['data']['list'])
-print('响应解析后的类型：',type(data))
-print('响应解析后的键值对个数：',len(data))
-for key in data: #打印出所有的keys
-    print(key ,end=' ')
+for i in range(1,100):
+    users = data['data']['list'][i]['user_name']
