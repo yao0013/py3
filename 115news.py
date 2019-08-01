3cd36a1b28d61253d853f2334be5c9d93529b0fc
yao0013 <yao0013@126.com>
2019-08-01 16:28:28 +0800

    1628

--- 115news.py
+++ 115news.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from pyecharts.charts import Bar
 headers = {
     'Cookie':'Hm_lvt_17965aa5d4a3ebae4c8b68932b9557a8=1562639526;acw_tc=784e2c9d15640478198377695e4e9913a130171b3a1b280a6f11b928b51dd3;UID=84838689_A1_1564619000;CID=5bb50052b339dd96a134d015b0f2743f;SEID=d78cb57f6d4365014af1dd328a04969047cbd3f8096913b50448855d7560ae7a1dd1bd0b9a8d67ef1051107c9bca460127b0802a57d21bdec3f0de36;'
@@ -8,8 +9,12 @@ url = 'https://yun.115.com/api/1.0/web/1.0/5/news/glist?start=0&limit=20&search_
 
 resq = requests.get(url=url,headers=headers)
 data = resq.json()
-user_name = data['data']['list']
-print(data['data']['list']['user_name'])
+users = data['data']['list']
+user = str(users)
+print(user)
+name = re.findall('user_name\':\'(.*?)\'\,\'is_member',user,re.S)
+print(name)
+print(data['data']['list'])
 print('响应解析后的类型：',type(data))
 print('响应解析后的键值对个数：',len(data))
 for key in data: #打印出所有的keys
