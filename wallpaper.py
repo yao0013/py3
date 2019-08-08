c105820a6943c4ceca38c4f9d83f823b1d30d0cc
yao0013 <myname@mymail.comyao0013@126.com>
2019-08-08 22:33:19 +0800

    ces

--- wallpaper.py
+++ wallpaper.py
@@ -2,16 +2,16 @@ import requests
 from lxml import etree
 
 def get_url(url):
-    url = "https://wallhaven.cc/toplist"
+
     html1 = requests.get(url).text
     html = etree.HTML(html1)
     #wallpaper_name = html.xpath(
     #    '//ul/li/figure/a/@title')
-    wallpaper_url = html.xpath('//ul/li/figure/a/@href')
+    wallpaper_urls = html.xpath('//ul/li/figure/a/@href')
     #print(wallpaper_url)
     return (wallpaper_urls)
 
-def get_magnet(wallpaper_urls):
+def get_down(wallpaper_urls):
 
     for wurl in wallpaper_urls:
 
@@ -21,13 +21,19 @@ def get_magnet(wallpaper_urls):
 
         download = html3.xpath('//section/div/img/@src')
         
+        resp = requests.get (download)
+        if not os.path.exists('./pics'):
+            os.makedirs('./pics')
+ 
+        with open('./pics/download','wb') as fp:
+            fp.write(resp.content)
 
 
 def main():
-    urls = [f'http://www.mp4ba.com/dianying/list_{i}.html' for i in range(1,10)]
+    urls = [f'https://wallhaven.cc/toplist?page={i}' for i in range(1,3)]
     for url in urls:
-        movies = get_url(url)
-        get_magnet(movies)
+        wallpaper_urls = get_url(url)
+        get_down(wallpaper_urls)
 
 if __name__ == '__main__':
-    main()'''
+    main()
