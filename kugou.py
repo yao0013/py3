2396f1591aeff860b27719300d6e15fa9ec51ef3
yao0013 <yao0013@126.com>
2019-08-16 14:11:12 +0800

    ces

--- kugou.py
+++ kugou.py
@@ -1,8 +1,37 @@
 import pymysql 
-db = pymysql.connect(host ='127.0.0.1' , user ='root' , password ='root', port=3306) 
+import requests
+from bs4 import BeautifulSoup
+import time
+ 
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
+}
+ 
+def get_info(url):
+    web_data = requests.get(url,headers=headers)
+    soup = BeautifulSoup(web_data.text,'lxml')
+    ranks = soup.select('span.pc_temp_num')
+    titles = soup.select('#rankWrap > div.pc_temp_songlist > ul > li > a')
+    times = soup.select('span.pc_temp_tips_r > span')
+    for rank,title,time in zip(ranks,titles,times):
+        data = {
+            'rank':rank.get_text().strip(),
+            'singer':title.get_text().split('-')[0],
+            'song':title.get_text().split('-')[1],
+            'time':time.get_text().strip()
+        }
+        print(data)
+if __name__ == '__main__':
+    urls = ['http://www.kugou.com/yy/rank/home/{}-8888.html?from=rank'.format(str(i))
+            for i in range(1,24)]
+    for url in urls:
+        get_info(url)
+        time.sleep(1)
+
+'''db = pymysql.connect(host ='127.0.0.1' , user ='root' , password ='root', port=3306) 
 cursor = db.cursor() 
 cursor.execute ('SELECT VERSION()') 
 data = cursor.fetchone() 
 print('Database version:', data) 
 cursor.execute ("CREATE DATABASE spiders DEFAULT CHARACTER SET utf8") 
-db.close()
+db.close()'''
