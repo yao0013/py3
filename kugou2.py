b07b261986f0f1bbb355fe25c74d48c213759542
yao0013 <myname@mymail.comyao0013@126.com>
2019-08-18 18:50:21 +0800

    1850

--- kugou2.py
+++ kugou2.py
@@ -12,7 +12,7 @@ headers = {
 def get_info(url):
     db = pymysql.connect(host ='127.0.0.1' , user ='root' , password ='root', port=3306,db='spiders') 
     cursor = db.cursor() 
-    sql = 'create table if not exists kugou3(ranks VARCHAR(255) NOT NULL, singer VARCHAR(255) NOT NULL, song VARCHAR(255) NOT NULL, time VARCHAR(255) NOT NULL)'
+    sql = 'create table if not exists kugoulist(ranks VARCHAR(255) NOT NULL, singer VARCHAR(255) NOT NULL, song VARCHAR(255) NOT NULL, time VARCHAR(255) NOT NULL)'
     cursor.execute(sql)
     web_data = requests.get(url,headers=headers)
     soup = BeautifulSoup(web_data.text,'lxml')
@@ -20,36 +20,29 @@ def get_info(url):
     titles = soup.select('#rankWrap > div.pc_temp_songlist > ul > li > a')
     times = soup.select('span.pc_temp_tips_r > span')
     for rank,title,time in zip(ranks,titles,times):
-        datas = {
+        data = {
             'ranks':rank.get_text().strip(),
             'singer':title.get_text().split('-')[0],
             'song':title.get_text().split('-')[1],
             'time':time.get_text().strip()
         }
-        print(datas)
-
-    data = [datas]
-    
         
-    table = 'kugou3'
-    for item in data:
-        keys = ','.join(item.keys())
-        values = ','.join(['%s'] * len(item))
+        table = 'kugoulist'
+        keys = ','.join(data.keys())
+        values = ','.join(['%s'] * len(data))
         sql = 'INSERT INTO {table}({keys}) VALUES ({values})'.format(table=table, keys=keys, values=values)
         try:
-            if cursor.execute(sql, tuple(item.values())):
-                db.commit()
+            if cursor.execute(sql, tuple(data.values())):
                 print('Successful')
-                
+                db.commit()
         except:
             print('Failed')
             db.rollback()
-
+    db.close()
 
 if __name__ == '__main__':
     urls = ['http://www.kugou.com/yy/rank/home/{}-8888.html?from=rank'.format(str(i))
             for i in range(1,24)]
     for url in urls:
         get_info(url)
-
-
+        time.sleep(1)
