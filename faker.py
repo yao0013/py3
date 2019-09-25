533140b534d704f44c119ac877cd3b28769b8c8c
yao0013 <myname@mymail.comyao0013@126.com>
2019-09-25 22:21:34 +0800

    faker使用

---
+++ faker.py
@@ -0,0 +1,16 @@
+import requests
+from faker import Faker
+
+f=Faker(locale='zh_CN')
+def add_member():
+    headers = {
+
+    }
+    data = {
+
+    }
+
+    url = ''
+
+    for i in range(1500):
+        resq = requests.post(url=url,data=data.headers=headers)
