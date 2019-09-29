14fbd7f033147d0081936b35e6b87f621c75f591
yao0013 <yao0013@126.com>
2019-09-29 16:27:36 +0800

    测试

--- regest.py
+++ regest.py
@@ -1,4 +1,4 @@
-import requests
+import requests,tkinter
 
 def regedit():
     name = str(input('请输入注册名'))
@@ -17,5 +17,29 @@ def regedit():
 
    
     print(f'注册码是{back}')
+
+    '''sr = input('谢谢使用')
+
+    while sr == 'q':
+        break'''
+window = tkinter.Tk
+window.title('注册机')
+window.geometry('400x400')
+button1 = tkinter.Button(window,
+                         text='生成注册码',
+                         bg='orange',
+                         height=3,
+                         width=20,
+                         bd=3,
+                         relief='sunken',
+                         activebackground='orange',
+                         activeforeground='white',
+                         command=action1
+                         )
+button1.pack()
+
+
+button2 = tkinter.Button(window, text='退出', height=3, command=window.quit())
+button2.pack()
 
 regedit()
