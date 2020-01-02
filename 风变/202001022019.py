56e59a9543105f50ba73652add22a9ebc6ab738a
yao0013 <myname@mymail.comyao0013@126.com>
2020-01-02 20:45:39 +0800

    1002

--- 风变/202001022019.py
+++ 风变/202001022019.py
@@ -1,42 +1,39 @@
 from gevent import monkey
-#从gevent库里导入monkey模块。
 monkey.patch_all()
-#monkey.patch_all()能把程序变成协作式运行，就是可以帮助程序实现异步。
-import gevent,time,requests
-#导入gevent、time、requests。
+import gevent,requests,bs4,csv
+from gevent.queue import Queue
 
-start = time.time()
-#记录程序开始时间。
+work = Queue()
 
-url_list = ['https://www.baidu.com/',
-'https://www.sina.com.cn/',
-'http://www.sohu.com/',
-'https://www.qq.com/',
-'https://www.163.com/',
-'http://www.iqiyi.com/',
-'https://www.tmall.com/',
-'http://www.ifeng.com/']
-#把8个网站封装成列表。
+url_1 = 'http://www.mtime.com/top/tv/top100/'
+work.put_nowait(url_1)
 
-def crawler(url):
-#定义一个crawler()函数。
-    r = requests.get(url)
-    #用requests.get()函数爬取网站。
-    print(url,time.time()-start,r.status_code)
-    #打印网址、请求运行时间、状态码。
+url_2 = 'http://www.mtime.com/top/tv/top100/index-{page}.html'
+for x in range(2,11):
+    real_url = url_2.format(page=x)
+    work.put_nowait(real_url)
 
-tasks_list = [ ]
-#创建空的任务列表。
+def crawler():
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    while not work.empty():
+        url = work.get_nowait()
+        res = requests.get(url,headers=headers)
+        bs_res = bs4.BeautifulSoup(res.text,'html.parser')
+        datas = bs_res.find_all('div',class_="mov_con")
+        for data in datas:
+            TV_title = data.find('a').text
+            data = data.find_all('p')
+            TV_data =''
+            for i in data:
+                TV_data =TV_data + ''+ i.text
+            writer.writerow([TV_title,TV_data])
+            print([TV_title,TV_data])
 
-for url in url_list:
-#遍历url_list。
-    task = gevent.spawn(crawler,url)
-    #用gevent.spawn()函数创建任务。
-    tasks_list.append(task)
-    #往任务列表添加任务。
-gevent.joinall(tasks_list)
-#执行任务列表里的所有任务，就是让爬虫开始爬取网站。
-end = time.time()
-#记录程序结束时间。
-print(end-start)
-#打印程序最终所需时间。
+csv_file = open('timetop.csv','w',newline='',encoding='utf-8')
+writer = csv.writer(csv_file)
+
+task_list = []
+for x in range(3):
+    task = gevent.spawn(crawler)
+    task_list.append(task)
+gevent.joinall(task_list)
