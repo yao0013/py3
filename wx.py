d3d8877316427e9f6cf43bfdff1773a183f14813
yao0013 <yao0013@126.com>
2020-05-11 17:32:05 +0800

    studyt

--- wx.py
+++ wx.py
@@ -3,6 +3,6 @@ import datetime as dt
 bot = Bot()
 
 while True:
-if dt.datetime.now().strftime('%H:%M') == '5:20':
-    my_friend = bot.friends().search('女神')[0]
-    my_friend.send('早安') 
+    if dt.datetime.now().strftime('%H:%M') == '5:20':
+        my_friend = bot.friends().search('低脂咖啡')[0]
+        my_friend.send('早安') 
