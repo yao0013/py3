f3daaa3856d34d352fb240b30ebc062027a4283b
yao0013 <yao0013@123.com>
2021-02-22 15:10:48 +0800

    test

--- 寒武纪/carilogin.py
+++ 寒武纪/carilogin.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from HTMLTestRunner import HTMLTestRunner
+
 
 options = webdriver.ChromeOptions()
 
