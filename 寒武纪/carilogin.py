10b5985cfe936ba89c4fbf9553a2bba9506e07b9
yao0013 <yao0013@123.com>
2021-02-22 16:27:32 +0800

    1

--- 寒武纪/carilogin.py
+++ 寒武纪/carilogin.py
@@ -2,7 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from HTMLTestRunner import HTMLTestRunner
+from HTMLTestRunner import HTMLTestReport
+
 
 
 options = webdriver.ChromeOptions()
@@ -21,18 +22,25 @@ user = driver.find_element_by_id('username')
 user.send_keys('yaocm@g-cloud.com.cn')
 password = driver.find_element_by_id('password')
 password.send_keys('gcloud123!@#')
-button = driver.find_element_by_xpath("/html/body[@class='userLayout']/div[@id='app']/div[@id='userLayout']/div[@class='container']/div[@class='main']/div[@class='wrapper']/div[@class='container']/form[@id='formLogin']/div[@class='ant-row ant-form-item']/div[@class='ant-col ant-form-item-control-wrapper']/div[@class='ant-form-item-control']/span[@class='ant-form-item-children']/button[@class='login-button btn-transition ant-btn ant-btn-primary ant-btn-round ant-btn-lg']")
+button = driver.find_element_by_xpath("/html//form[@id='formLogin']/div[5]//button[@type='submit']")
 button.click()
 time.sleep(5)
 '''users = driver.find_element_by_xpath("/html/body/div[@id='app']/section[@class='layout bg-color ant-layout']/section[@class='ant-layout bg-color sidemenu content-width-Fixed']/header[@class='ant-layout-header ant-header-side-opened']/div[@class='header']/div[@class='user-wrapper vertical-center']/a[@class='header-btn vertical-center']/span[@class='header-underline-btn']").text'''
 time.sleep(5)
-r1 = EC.title_is('寒武纪CAIR平台')(driver)
-r2 = EC.title_contains('寒武纪CAIR平台')(driver)
-r3 = EC.text_to_be_present_in_element((By.CLASS_NAME,'pr6'),'管理员')(driver)
+r1 = EC.title_is('Cambricon AI Repo')(driver)
+r2 = EC.title_contains('Cambricon AI Repo')(driver)
+r3 = EC.text_to_be_present_in_element((By.CLASS_NAME,'pr6'),'塔比特')(driver)
 print (r1)
 print (r2)
 print (r3)
 
+with open('./HwTestReport.html', 'wb') as report:
+        runner = HTMLTestReport(stream=report,
+                                verbosity=2,
+                                title='HwTestReport 测试',
+                                description='带饼图，带详情',
+                                tester='Johnny')
+        '''runner.run(r1,r2,r3)'''
 driver.quit()
 
 '''if users == user2
